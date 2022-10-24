7a9c9ef83b269098d65f9ac39dfd9788c3159bd5
Sana Homaei <sana.homaei@beorntech.com>
2022-10-24 12:17:48 +0200

    Added some fragments

---
+++ fragments/intranet-fragments/src/intranet/overview/main.js
@@ -0,0 +1,4 @@
+console.group('Overview');
+console.log('fragmentElement', fragmentElement);
+console.log('configuration', configuration);
+console.groupEnd();
---
+++ fragments/intranet-fragments/src/intranet/tile-task/main.js
@@ -0,0 +1,4 @@
+console.group('Tile-task');
+console.log('fragmentElement', fragmentElement);
+console.log('configuration', configuration);
+console.groupEnd();
